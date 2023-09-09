57d846662b7ff2b30a4ae26a9126cf4521126c42
musesum <musesum@gmail.com>
2023-09-09 14:01:21 -0700

    MuFlo branch main

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["MuMetal"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/musesum/MuFlo.git", from: "0.23.0"),
+        .package(url: "https://github.com/musesum/MuFlo.git", .branch("main")),
     ],
     targets: [
         .target(
